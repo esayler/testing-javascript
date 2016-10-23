eef4ed7440020dc6322568b3bfe37f1a2b98cdf5
Eric Sayler <easayler@gmail.com>
2016-10-23 10:10:04 -0600

    add basic tests and implementations

--- implementation.js
+++ implementation.js
@@ -2,3 +2,32 @@
 function square(n) {
   return n * n;
 }
+
+function addTwo(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function fizBuzz(num) {
+  if ( num % 3 === 0 && num % 5 === 0 ) {
+    return 'FizzBuzz';
+  } else if ( num % 3 === 0 ) {
+    return 'Fizz';
+  } else if (num % 5 === 0){
+    return 'Buzz';
+  } else {
+    return num;
+  }
+}
+
--- test.js
+++ test.js
@@ -5,3 +5,54 @@ describe('square', function () {
   });
 
 });
+
+describe('addTwo', function () {
+
+  it('returns the sum of two numbers', function () {
+    assert.equal(addTwo(2, 5), 7);
+  });
+
+});
+
+describe('multiply', function () {
+
+  it('returns the product of two numbers', function () {
+    assert.equal(multiply(3, 4), 12);
+  });
+
+});
+
+describe('subtract', function () {
+
+  it('returns the difference of two numbers', function () {
+    assert.equal(subtract(10, 5), 5);
+  });
+
+});
+
+describe('divide', function () {
+
+  it('returns the quotient of two numbers', function () {
+    assert.equal(divide(10, 5), 2);
+  });
+
+});
+
+describe('fizBuzz', function () {
+  it('returns the number passed to it', function () {
+    assert.equal(fizBuzz(1), 1);
+  });
+
+  it('returns "Fizz" if the number passed to it is a multiple of 3', function () {
+    assert.equal(fizBuzz(3), 'Fizz');
+  });
+
+  it('returns "Buzz" if the number passed to it is a multiple of 5', function () {
+    assert.equal(fizBuzz(5), 'Buzz');
+  });
+
+  it('returns "FizzBuzz" if the number passed to it is a multiple of 3 and 5', function () {
+    assert.equal(fizBuzz(15), 'FizzBuzz');
+  });
+
+});
